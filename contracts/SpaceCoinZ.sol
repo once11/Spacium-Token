7d1577caf2a0b14e5a751c898d9827be8a40200e
Agustin Abreu <agustinabreu@gmail.com>
2017-07-25 16:23:34 -0300

    SpaceCoinZ contract based on ERC 20

---
+++ contracts/SpaceCoinZ.sol
@@ -0,0 +1,11 @@
+/*
+SpaceCoinZ token
+Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
+.*/
+pragma solidity ^0.4.4;
+
+import "tokens/StandardToken.sol";
+
+contract SpaceCoinZ is StandardToken {
+
+}
